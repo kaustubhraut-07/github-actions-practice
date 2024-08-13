5ef1da134bcdc836e05b2816cb9ca1d33164a008
Kaustubh Raut <kaustubh.raut@fxis.ai>
2024-08-13 18:10:31 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ const App = () => {
   return (
     <div>
       <p>
-        checking the workflow file and updated changes added correct path
-
+        checking the workflow file and updated changes added correct path 1
       </p>
     </div>
   )
